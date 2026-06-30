b5e1a23c6547046e8c325bbd6f2eb56290c5e5c5
DylanLewis <lewisdylann7@gmail.com>
2026-06-30 08:59:08 -0400

    Add observability module for serverless platform

---
+++ modules/observability/variables.tf
@@ -0,0 +1,25 @@
+variable "resource_group_name" {
+    description = "Resource Group Name"
+    type = string  
+}
+
+variable "location" {
+    description = "Region"
+    type = string  
+}
+
+variable "log_analytics_workspace_name" {
+    description = "Log analytics workspace name"
+    type = string  
+}
+
+variable "application_insights_name" {
+    description = "Application insights name"
+    type = string  
+}
+
+variable "tags" {
+    description = "Tags"
+    type = map(string)
+    default = {}  
+}
