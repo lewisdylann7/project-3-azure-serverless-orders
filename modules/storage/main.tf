77259285c20f5258065a2113b5e157e1da7b5edf
DylanLewis <lewisdylann7@gmail.com>
2026-06-30 09:17:26 -0400

    Add storage module for function runtime and processed order data

---
+++ modules/storage/main.tf
@@ -0,0 +1,18 @@
+resource "azurerm_storage_account" "main" {
+    name = var.storage_account_name
+    resource_group_name = var.resource_group_name
+    location = var.location
+    account_tier =  "Standard"
+    account_replication_type = "LRS"
+    min_tls_version = "TLS1_2"
+    public_network_access_enabled = true
+    shared_access_key_enabled = true
+    allow_nested_items_to_be_public = false
+    tags = var.tags
+}
+
+resource "azurerm_storage_container" "orders" {
+    name = "processed-orders"
+    storage_account_id = azurerm_storage_account.main.id
+    container_access_type = "private"
+}
